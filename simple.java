f0bdb21455362ca66f2489a04879cbdadd3dfd19
ftem2002 <91579449+ftem2002@users.noreply.github.com>
2021-12-24 00:15:29 +0300

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,16 @@
+
+public class simple {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+int a,b,c;
+
+a=10;
+b=15;
+c=a+b;
+System.out.print(c);		
+		
+	}
+
+}
